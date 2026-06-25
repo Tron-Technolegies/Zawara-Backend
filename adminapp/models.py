a4e117769341845a34340d3b7a9bfac080662cbf
Sanjaydas <sanjaydasps0@gmail.com>
2026-06-25 15:20:47 +0530

    cart completed

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    # image = CloudinaryField("category", blank=True, null=True)
+    image = CloudinaryField("category", blank=True, null=True)
     description = models.TextField(blank=True)
     status = models.CharField(
         max_length=10,
@@ -27,7 +28,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     stock = models.PositiveIntegerField(default=0)
-    # image = CloudinaryField("image", blank=True, null=True)
+    image = CloudinaryField("image", blank=True, null=True)
     # materials = models.ManyToManyField(Material, blank=True, related_name="products")
 
 
