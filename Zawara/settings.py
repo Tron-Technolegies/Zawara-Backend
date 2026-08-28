a34dd7f1344c8b347c81eafd9eadefdebbba95ab
savithnarayan111 <savithnarayan111@gmail.com>
2026-08-28 15:16:43 +0530

    created superuser

--- Zawara/settings.py
+++ Zawara/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
-    "zawara-backend.onrender.com",
+    "zawara-backend-23bb.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
